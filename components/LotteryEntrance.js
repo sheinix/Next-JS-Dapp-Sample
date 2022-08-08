b501ec9f314f983aed43a0a5f795e63d59605427
sheinix <jnuvreni@gmail.com>
2022-08-08 17:01:56 +1200

    Added userEffects and notifications to frontend

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,27 +1,60 @@
-import {useWeb3Contract} from "react-moralis"
-import {abi, contractAddresses} from "../constants"
-import {useMoralis} from "react-moralis"
-import {useEffect, useState} from "react"
+import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
+import { useMoralis } from "react-moralis"
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
+import { useNotification, userNotification } from "web3uikit"
 
 export default function LotteryEntrance() {
-    const {chainId: chainIdHex, isWeb3Enabled} = useMoralis()
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
     const chainId = parseInt(chainIdHex)
     const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
     const [entranceFee, setEntranceFee] = useState("0")
+    const [numPlayers, setNumPlayers] = useState("0")
+    const [recentWinner, setRecentWinner] = useState("0")
 
-    const {runContractFunction: enterRaffle} = useWeb3Contract({
+    const dispatch = useNotification()
+
+    const { runContractFunction: enterRaffle } = useWeb3Contract({
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "enterRaffle",
         msgValue: entranceFee,
-        params: {}
+        params: {},
     })
 
-    const {runContractFunction: getEntranceFee} = useWeb3Contract({abi: abi, contractAddress: raffleAddress, functionName: "getEntranceFee", params: {}})
+    console.log("ABI:" + abi)
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getNumberOfPlayer",
+        params: {},
+    })
+
+    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getRecentWinner",
+        params: {},
+    })
 
     async function updateUI() {
-        const entranceFeeFromCall = await getEntranceFee()
-        setEntranceFee(entranceFeeFromCall.toString())
+        const entranceFeeFromCall = (await getEntranceFee()).toString()
+        const numPlayersFromCall = (await getNumberOfPlayers()).toString()
+        const recentWinnerFromCall = (await getRecentWinner()).toString()
+
+        setEntranceFee(entranceFeeFromCall)
+        setNumPlayers(numPlayersFromCall)
+        setRecentWinner(recentWinnerFromCall)
+
         console.log(entranceFee)
     }
 
@@ -31,10 +64,46 @@ export default function LotteryEntrance() {
         }
     }, [isWeb3Enabled])
 
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNewNotification(tx)
+    }
+
+    const handleNewNotification = function () {
+        dispatch({
+            type: "info",
+            message: "Transaction Complete",
+            title: "Tx Notification",
+            position: "topR",
+            icon: "bell",
+        })
+        updateUI()
+    }
+
     return (
         <div>
             Hi From lottery entrance!
-            <div>{entranceFee}</div>
+            {raffleAddress ? (
+                <div>
+                    <button
+                        onClick={async () =>
+                            await enterRaffle({
+                                onSuccess: handleSuccess,
+                                onError: (error) => console.error(error),
+                            })
+                        }
+                    >
+                        Enter Raffle
+                    </button>
+                    <br />
+                    Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH <br />
+                    Number of Players: {numPlayers}
+                    <br />
+                    Recent Winner: {recentWinner}
+                </div>
+            ) : (
+                <div>No Raffle Address Detected</div>
+            )}
         </div>
     )
 }
